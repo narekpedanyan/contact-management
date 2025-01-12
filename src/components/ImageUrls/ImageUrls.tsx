463179c149cc0c62b87ac92121d7bdd77ad178c3
narekpedanyan <narek@corpora.us>
2025-01-13 02:54:31 +0400

    Oops

---
+++ src/components/ImageUrls/ImageUrls.tsx
@@ -0,0 +1,23 @@
+const ImageUrls = () => {
+    return (
+        <div className="mx-auto p-4 border rounded shadow-md bg-white mt-8">
+            <h2 className="text-xl font-semibold text-gray-800 mb-4">Copy Image URL for testing</h2>
+            <div className="space-y-2 mb-4">
+                <div className="block text-blue-500 underline hover:text-blue-700 text-sm">
+                    https://images.unsplash.com/photo-1636041246170-9278569b9c36?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D
+                </div>
+                <div className="block text-blue-500 underline hover:text-blue-700 text-sm">
+                    https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D
+                </div>
+                <div className="block text-blue-500 underline hover:text-blue-700 text-sm">
+                    https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=3307&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+                </div>
+                <div className="block text-blue-500 underline hover:text-blue-700 text-sm">
+                    https://plus.unsplash.com/premium_photo-1689551670902-19b441a6afde?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImageUrls;
